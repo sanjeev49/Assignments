3945177395309e1b4da1468e6e5b2048ce4405f4
unknown <sanjeevkr4983@gmail.com>
2021-08-28 11:55:27 +0530

    started machine learning

--- Class lectures/flask_demo/app.py
+++ Class lectures/flask_demo/app.py
@@ -3,30 +3,6 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST']) # To render Homepage
-def home_page():
-    return render_template('index.html')
-
-@app.route('/math', methods=['POST'])  # This will be called from UI
-def math_operation():
-    if (request.method=='POST'):
-        operation=request.form['operation']
-        num1=int(request.form['num1'])
-        num2 = int(request.form['num2'])
-        if(operation=='add'):
-            r=num1+num2
-            result= 'the sum of '+str(num1)+' and '+str(num2) +' is '+str(r)
-        if (operation == 'subtract'):
-            r = num1 - num2
-            result = 'the difference of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
-        if (operation == 'multiply'):
-            r = num1 * num2
-            result = 'the product of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
-        if (operation == 'divide'):
-            r = num1 / num2
-            result = 'the quotient when ' + str(num1) + ' is divided by ' + str(num2) + ' is ' + str(r)
-        return render_template('results.html',result=result)
-
 @app.route('/via_postman', methods=['POST']) # for calling the API from Postman/SOAPUI
 def math_operation_via_postman():
     if (request.method=='POST'):
@@ -47,6 +23,29 @@ def math_operation_via_postman():
             result = 'the quotient when ' + str(num1) + ' is divided by ' + str(num2) + ' is ' + str(r)
         return jsonify(result)
 
+@app.route('/sanjeev2', methods=['POST'])  # for calling the API from Postman/SOAPUI
+def math_operation_via_postman2():
+    if request.method == "POST":
+        num1 = int(request.json['num1'])
+        num2 = int(request.json['num2'])
+        result2 = num1 + num2
+        return jsonify(result2)
+
+@app.route('/sanjeev3',methods = ['POST'])
+def math_operation():
+    if request.method == 'POST':
+        name = request.json['name']
+        email = request.json['mail']
+        phone  = request.json['number']
+        return jsonify(name+email+phone)
+@app.route('/sudh', methods = ['POST'])
+def math_sudh():
+    if (request.method == 'POST'):
+        num_0 = request.json['num0']
+        num1 = int(request.json['num1'])
+        num2 = int(request.json['num2'])
+        result = num_0 + num2 + num1
+        return jsonify(result                     )
 
 if __name__ == '__main__':
     app.run()
